cd516ae2e9cea23477e4715aa3d042528b44421d
Arka <146548597+Arkadeep18@users.noreply.github.com>
2024-01-04 00:31:23 +0530

    Update Prac7.java

--- Prac7.java
+++ Prac7.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Prac7 {
 
 	public static void main(String[] args) {
-		
+		//Even/Odd
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter value of x");
 		double x = sc.nextDouble();
