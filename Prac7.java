0829e5ada3a9e4990724d910a0110222058bf668
Arka <146548597+Arkadeep18@users.noreply.github.com>
2024-01-04 00:30:24 +0530

    Add files via upload

---
+++ Prac7.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Prac7 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter value of x");
+		double x = sc.nextDouble();
+		
+		if(x%2 == 0) {
+			System.out.println("Even");
+		}
+		else 
+		{System.out.println("Odd");
+		
+		}
+	}
+
+}
